593a813ff43a28ad041ed5b5336beb6b245c432b
Juan Pedro Villa Isaza <jpvillaisaza@gmail.com>
2016-09-10 16:35:05 -0500

    Restructure the library for v0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# mailchimp-haskell
+# Haskell bindings for the MailChimp API
+
+[![][1]][0]
+
+[0]: https://travis-ci.org/jpvillaisaza/mailchimp-haskell
+[1]: https://travis-ci.org/jpvillaisaza/mailchimp-haskell.svg?branch=master
+
+![][2]
+![][3]
+
+[2]: https://www.stackage.org/package/mailchimp/badge/lts
+[3]: https://www.stackage.org/package/mailchimp/badge/nightly
+
+## License
+
+Licensed under the MIT license. See [LICENSE.md](LICENSE.md).
+
+[mailchimp]: http://mailchimp.com/
+[mailchimp-api]: http://developer.mailchimp.com/
--- src/Web/MailChimp/Tutorial.hs
+++ src/Web/MailChimp/Tutorial.hs
@@ -1,39 +1,56 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-
 ----------------------------------------------------------------------
 -- |
 -- Module: Web.MailChimp.Tutorial
+-- Description:
 --
 --
 --
 ----------------------------------------------------------------------
 
 module Web.MailChimp.Tutorial
+  (
+  )
   where
 
--- mailchimp
-import Web.MailChimp
-
--- aeson
-import Data.Aeson
-
-
-example :: IO ()
-example = do
-  manager <- makeManager
-  let
-    key = ""
-    listId = "3b736efca9"
-    member =
-      (makeListMemberRequest "sd@sd.com" "pending")
-        {listMemberMergeFields = [("FNAME", "Juan")]
-        , listMemberExtra = [("vip", toJSON True)]}
-  let Just Client{..} = makeClient manager key
-  let ListClient{..} = makeListClient listId
---  let ListMemberClient{..} = listMemberClient
-  let Just ListMemberClient{..} = makeListMemberClient manager key listId
-  res <- addListMember member
-  case res of
-    Left err -> putStrLn $ "Error: " ++ show err
-    Right msg -> print msg
+-- |
+--
+-- {-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE RecordWildCards #-}
+-- 
+-- -- base
+-- import System.Environment (getEnv)
+--
+-- -- bytestring
+-- import Data.ByteString.Char8 (pack)
+--
+-- -- mailchimp
+-- import Web.MailChimp
+--
+-- -- text
+-- import qualified Data.Text as Text
+--
+--
+-- example :: IO ()
+-- example = do
+--   manager <- makeManager
+--   key <- fmap pack (getEnv "MAILCHIMP_API_KEY")
+--   listId <- fmap Text.pack (getEnv "MAILCHIMP_LIST_ID")
+--
+--   let
+--     Just ListMemberClient {..} =
+--       makeListMemberClient manager key listId
+--
+--   let
+--     member =
+--       (makeListMemberRequest "sd@sd.com" Pending)
+--         { listMemberMergeFields = [("FNAME", "Juan")]
+--         }
+--
+--   eitherAdd <- addListMember member
+--
+--   case eitherAdd of
+--     Left err ->
+--       putStrLn $ "Error: " ++ show err
+--
+--     Right msg ->
+--       print msg
